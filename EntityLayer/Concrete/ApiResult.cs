cc5605e7efb2cf59c67aac9f84025f7fba7e4b2d
karun <1harunkorkmaz@gmail.com>
2024-05-10 17:04:32 +0300

    o

---
+++ EntityLayer/Concrete/ApiResult.cs
@@ -0,0 +1,18 @@
+
+public class ApiResult : ApiResult<object>
+{
+
+}
+
+public class ApiResult<T>
+{
+    public ApiResult(bool isSuccess = true, object message = null)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+    }
+    public bool IsSuccess { get; set; }
+    public T Data { get; set; }
+    public object Message { get; set; }
+
+}
